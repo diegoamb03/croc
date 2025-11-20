2ff7355308428b7507acc0a6ecded3e6d6bd996d
Diego Beltran <68976175+diegoamb03@users.noreply.github.com>
2025-11-20 14:14:54 -0500

    Delete Sicoe directory

--- Sicoe/sicoe_detail_sales.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Iniciando programador SICOE...
-cd /d "C:\Users\Lenovo\Downloads\scripts\Sicoe"
-"C:\Users\Lenovo\AppData\Local\Programs\Python\Python313\python.exe" "programador_sicoe.py"
-echo Programador finalizado a las %date% %time% >> log_programador.txt
-pause
